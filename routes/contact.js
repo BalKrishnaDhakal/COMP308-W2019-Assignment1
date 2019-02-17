7f01ef593d2ec6f73be3a03af9c5026dd84d8b3b
Bal krishna Dhakal <bdhakal1@my.centennialcollege.ca>
2019-02-16 21:08:07 -0500

    added author name and descriptions

--- routes/contact.js
+++ routes/contact.js
@@ -1,3 +1,11 @@
+/*
+    Student Name: Bal Krishna Dhakal
+    Student ID: 300916314
+    Description: COMP308-W2019-Assignment1
+    Date Created: February 16, 2019
+    Link to GitHub: https://github.com/BalKrishnaDhakal/COMP308-W2019-Assignment1
+*/
+
 let express = require('express');
 let router = express.Router();
 
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,12 @@
+/*
+    Student Name: Bal Krishna Dhakal
+    Student ID: 300916314
+    Description: COMP308-W2019-Assignment1
+    Date Created: February 16, 2019
+    Link to GitHub: https://github.com/BalKrishnaDhakal/COMP308-W2019-Assignment1
+*/
+
+
 let express = require('express');
 let router = express.Router();
 
@@ -5,11 +14,12 @@ let router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Home' });
 });
+/* GET about page. */
 router.get('/about', function(req, res, next) {
-    res.render('about', { title: 'About' });
+    res.render('about', { title: 'About Me' });
 });
 
-/* GET about page. */
+/* GET projects page. */
 router.get('/projects', function(req, res, next) {
     res.render('projects', { title: 'Projects' });
 });
@@ -17,6 +27,7 @@ router.get('/projects', function(req, res, next) {
 router.get('/services', function(req, res, next) {
     res.render('services', { title: 'Services' });
 });
+/* GET contact page. */
 router.get('/contact', function(req, res, next) {
     res.render('contact', { title: 'Contact' });
 });
